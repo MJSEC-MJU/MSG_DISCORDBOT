79c2668f6e6f3b7b25c95a0429e4ac80ff31a42f
walnutpy <leonjuwon@gmail.com>
2025-02-07 21:09:10 +0900

    Delete token.env

--- 이주원/역할봇.py
+++ 이주원/역할봇.py
@@ -1,35 +1,43 @@
 import discord, pytz, datetime
 from discord.ext import commands,tasks
-from itertools import cycle
 from discord.utils import get
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+DISCORD_SERVER_ID = os.getenv("DISCORD_SERVER_ID")
 
 bot = commands.Bot(command_prefix='/',intents=discord.Intents.all()) #명령어
 
-DISCORD_CHANNEL_ID = 1330808882993561703
-DISCORD_BOT_TOKEN = ''
+
 #토큰,서버아이디
 
+#이모지별 대학교
 user_role = ""
-bot_status = cycle(["멍때리기", "밥먹기", "게임", "열심히 코딩","열심을 다해 쉬기"])
-ROLE_EMOJI_DIC={"\U00000031\U000020E3":"***명지대학교***","\U00000032\U000020E3":"***00대학교***","\U00000033\U000020E3":"***00대학교***"}
+ROLE_EMOJI_DIC={"\U00000031\U000020E3":"명지대학교",
+                "\U00000032\U000020E3":"00대학교",
+                "\U00000033\U000020E3":"01대학교"}
 
-@tasks.loop(seconds=300)
-async def change_status():
-    await bot.change_presence(activity=discord.Game(next(bot_status)))
-
+#봇 시작 알림
 @bot.event
 async def on_ready():
     print("Bot is connecting to Discord")
+    guild = bot.get_guild(DISCORD_SERVER_ID)
+    for role_name in ROLE_EMOJI_DIC.values():
+        if not discord.utils.get(guild.roles,name=role_name):
+            await guild.create_role(name=role_name)
     channel = bot.get_channel(DISCORD_CHANNEL_ID)
-    change_status.start()
     if channel is None:
         channel.send('NO CHANNEL')
         return
     await channel.send('CONNECTED')
 
+
 @bot.event
 async def on_message(message):
-    if message.content == "/우승자":
+    if message.content == "/우승자":#우승자 알림
         embed = discord.Embed(title="코딩 대회 우승자", timestamp=datetime.datetime.now(pytz.timezone('UTC')),color=0x00ff00)
         embed.add_field(name="유저네임",value=":trophy: 제 1회 우승자",inline=False)
         embed.add_field(name="유저네임",value=":trophy: 제 2회 우승자",inline=False)
@@ -38,7 +46,7 @@ async def on_message(message):
         channel = bot.get_channel(DISCORD_CHANNEL_ID)
         await message.channel.send (embed=embed)
 
-    if message.content.startswith ("/공지"):
+    if message.content.startswith ("/공지"):#공지 작성
         i = (message.author.guild_permissions.send_messages)
         if i is True:
             notice = message.content[4:]
@@ -49,7 +57,7 @@ async def on_message(message):
         if i is False:
             await message.channel.send("{}, 당신은 관리자가 아닙니다.".format(message.author))
 
-    if message.content == "/역할공지":
+    if message.content == "/역할공지":#역할지급 공지 한번만 공지해두면 실행시키고나서 이전 메시지에 이모지를 달아도 역할을 지급해줌
         channel = bot.get_channel(DISCORD_CHANNEL_ID)
         embed = discord.Embed(title="***역할지급***",description="아래 이모티콘을 클릭하여 역할을 받으세요! \n\n"+"\n".join([f"{emoji} : {role}"for emoji, role in ROLE_EMOJI_DIC.items()]),color=discord.Color.blue())
         ctx = await message.channel.send(embed=embed)
@@ -59,8 +67,58 @@ async def on_message(message):
             except discord.HTTPException as e:
                 print("ERROR")
 
-@bot.event
+@bot.event#역할지급
 async def on_raw_reaction_add(payload):
-    pass
+    if payload.user_id == bot.user.id:
+        return
+    
+    guild = bot.get_guild(payload.guild_id)
+    member = guild.get_member(payload.user_id)
+
+    emoji = payload.emoji.name
+    role_name = ROLE_EMOJI_DIC.get(emoji)
+
+    if role_name:
+        role = discord.utils.get(guild.roles, name = role_name)
+
+        existing_roles = [discord.utils.get(guild.roles,name = r)for r in ROLE_EMOJI_DIC.values()]
+        user_has_role = any(r in member.roles for r in existing_roles)
+        channel = bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+
+        if user_has_role:
+            await member.send("역할은 하나만 선택할 수 있습니다.")
+            try:
+                await message.remove_reaction(payload.emoji, member)
+            except discord.Forbidden:
+                print("봇이 반응을 제거할 권한이 없습니다.")
+            return
+
+        if role:
+            try:
+                await member.add_roles(role)
+            except discord.Forbidden:
+                print("역할을 추가할 수 없습니다.")
+            try:
+                await member.send(f"{role} 역할이 지급되었습니다.")
+            except discord.Forbidden:
+                print(f"{member}에게 DM을 보낼 수 없습니다")
+
+@bot.event #역할 삭제
+async def on_raw_reaction_remove(payload):
+    guild = bot.get_guild(payload.guild_id)
+    member = guild.get_member(payload.user_id)
+
+    emoji = payload.emoji.name
+    role_name = ROLE_EMOJI_DIC.get(emoji)
+
+    if role_name:
+        role = discord.utils.get(guild.roles,name=role_name)
+        if role:
+            await member.remove_roles(role)
+            try:
+                await member.send(f"{role} 역할이 삭제되었습니다.")
+            except discord.Forbidden:
+                print(f"{member}에게 DM을 보낼 수 없습니다")
 
 bot.run(DISCORD_BOT_TOKEN)
